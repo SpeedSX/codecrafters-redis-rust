2b10643747595ccfa231f34c081665b6f09250aa
Oleksii Rodionov <orodionov@emergemarket.com>
2026-04-03 14:53:15 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,18 @@ fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // Uncomment the code below to pass the first stage
-    //
+    const BUFFER_LENGTH: usize = 1024;
+
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    let mut buffer: Vec<u8> = Vec::new();
     
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
-                _stream.read_to_end(&mut buffer).unwrap();
+                let mut buffer: Vec<u8> = vec![0; BUFFER_LENGTH];
+                let len = _stream.read(&mut buffer[..]).unwrap();
                 
-                let message = String::from_utf8_lossy(&buffer);
-
-                for _command in message.split_whitespace() {
+                if len > 0 {
+                    println!("Received: {}", String::from_utf8_lossy(&buffer[..len]));
                     _stream.write_all(b"+PONG\r\n").unwrap();
                 }
             }

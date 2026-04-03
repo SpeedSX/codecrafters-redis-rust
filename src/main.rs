1f23f8be7f75245418539855cd1773037c5f54f5
Oleksii Rodionov <orodionov@emergemarket.com>
2026-04-03 14:42:24 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![allow(unused_imports)]
 use std::net::TcpListener;
-use std::io::Write;
+use std::io::{Write, Read};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -9,11 +9,18 @@ fn main() {
     // Uncomment the code below to pass the first stage
     //
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let mut buffer: Vec<u8> = Vec::new();
     
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
-                _stream.write_all(b"+PONG\r\n").unwrap()
+                _stream.read_to_end(&mut buffer).unwrap();
+                
+                let message = String::from_utf8_lossy(&buffer);
+
+                for _command in message.split_whitespace() {
+                    _stream.write_all(b"+PONG\r\n").unwrap();
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
